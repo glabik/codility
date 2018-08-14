1c7e57c6770068b905e842106c8fc9292b02a8e0
Marcin <Marcin@10.1.1.131>
2018-08-14 15:44:55 +0200

    implementation of lesson1

---
+++ codility/src/main/java/pl/com/glabik/codility/lesson1/Solution.java
@@ -0,0 +1,61 @@
+package pl.com.glabik.codility.lesson1;
+
+/*
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends 
+in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
+The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. 
+The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+	class Solution { public int solution(int N); }
+that, given a positive integer N, returns the length of its longest binary gap. 
+The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 
+and so its longest binary gap is of length 5. Given N = 32 the function should return 0, 
+because N has binary representation '100000' and thus no binary gaps.
+
+Assume that:
+        N is an integer within the range [1..2,147,483,647].
+
+Complexity:
+        expected worst-case time complexity is O(log(N));
+        expected worst-case space complexity is O(1).
+
+Copyright 2009–2018 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited. 
+ * */
+
+public class Solution {
+
+	public int solution(int N) {
+        String bs = Integer.toBinaryString(N);
+        boolean gapOpened = false;
+        int currentGapLength = 0;
+        int maxGapLength = 0;
+        
+        for(int i=0; i<bs.length(); i++) {
+        	if(!gapOpened) {
+        		if('1'== bs.charAt(i)){
+        			gapOpened = true;
+        		}	
+        	}else{
+        		if('1' == bs.charAt(i)){
+        			if(currentGapLength>maxGapLength) {
+        				maxGapLength = currentGapLength;
+        			}
+        			currentGapLength = 0;
+        		}else {
+        			currentGapLength++;
+        		}
+        	}
+        }
+        return maxGapLength;
+    }
+	
+	
+	
+}
---
+++ codility/src/test/java/pl/com/glabik/codility/lesson1/SolutionTest.java
@@ -0,0 +1,51 @@
+package pl.com.glabik.codility.lesson1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SolutionTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SolutionTest( String testName ){
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite(){
+        return new TestSuite( SolutionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testForValues(){
+    	Solution solution = new Solution();
+    	
+        assertEquals(2, solution.solution(9));
+        
+        assertEquals(4, solution.solution(529));
+        
+        assertEquals(1, solution.solution(20));
+        
+        assertEquals(0, solution.solution(15));
+        
+        assertEquals(0, solution.solution(32));
+        
+        assertEquals(0, solution.solution(1024));
+        
+        assertEquals(1, solution.solution(-33));
+        
+//        System.out.println(Integer.toBinaryString(2_147_483_648));
+    }
+    
+}
